490e2c58c9a66e0240c56c3c7e473655c1856a47
Ân Lê (FSOFT PC) <lehoaian.it@gmail.com>
2012-06-07 14:32:37 +0900

    Added Cities API for get a list of cities

---
+++ src/com/lehoaian/core/CitiesApi.java
@@ -0,0 +1,14 @@
+package com.lehoaian.core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LETHANHH
+ * Date: 6/7/12
+ * Time: 2:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CitiesApi {
+    public String[][] getListOfCities(){
+        return new String[][]{};
+    }
+}
