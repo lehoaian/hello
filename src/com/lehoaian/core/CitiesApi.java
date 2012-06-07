9ef26b6744f41dadbd4a8b95acb5d015e6c53d24
Ân Lê (FSOFT PC) <lehoaian.it@gmail.com>
2012-06-07 15:54:15 +0900

    Added Test class

--- src/com/lehoaian/core/CitiesApi.java
+++ src/com/lehoaian/core/CitiesApi.java
@@ -1,5 +1,11 @@
 package com.lehoaian.core;
 
+import android.content.Context;
+import android.util.Log;
+import com.lehoaian.hotel.R;
+import org.apache.http.client.methods.HttpGet;
+import org.json.JSONObject;
+
 /**
  * Created with IntelliJ IDEA.
  * User: LETHANHH
@@ -8,7 +14,29 @@ package com.lehoaian.core;
  * To change this template use File | Settings | File Templates.
  */
 public class CitiesApi {
-    public String[][] getListOfCities(){
+    Context _context;
+    String _apiUrl;
+    final static String TAG = "API";
+
+    public CitiesApi(Context context){
+        this._context = context;
+        _apiUrl = context.getString(R.string.api_json_all_cities);
+    }
+
+    public JSONObject getResponse(){
+        try {
+            ApiHelper.prepareUserAgent(_context);
+            Log.i(TAG, _apiUrl);
+            String response = ApiHelper.getUrlContent(_apiUrl);
+            Log.i(TAG, response);
+        } catch (ApiHelper.ApiException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        return new JSONObject();
+    }
+
+    public String[][] getListOfCities(int refreshFlg){
         return new String[][]{};
     }
 }
---
+++ src/com/lehoaian/core/CitiesApiTest.java
@@ -0,0 +1,17 @@
+package com.lehoaian.core;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LETHANHH
+ * Date: 6/7/12
+ * Time: 3:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CitiesApiTest {
+    @Test
+    public void testGetResponse() throws Exception {
+
+    }
+}
